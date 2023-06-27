2b4e45c128a14ec45c0d9a8d39c4d8c7eee51ac8
ponochovny <ponochovny@gmail.com>
2023-06-27 04:29:56 +0300

    feat: content

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+'use client'
+
+import { CustomButton } from '@/components'
 
 const Hero = () => {
-	return <div>Hero</div>
+	const handleScroll = () => {}
+
+	return (
+		<div className='hero'>
+			<div className='flex-1 pt-36 padding-x'>
+				<h1 className='hero__title'>
+					Find, book, or rent a car — quickly and easily!
+				</h1>
+
+				<p className='hero__subtitle'>
+					Streamline your car rental experience with out effortless booking
+					process.
+				</p>
+
+				<CustomButton
+					title='Expore Cars'
+					containerStyles='bg-primary-blue text-white rounded-full mt-10'
+					handleClick={handleScroll}
+				/>
+			</div>
+		</div>
+	)
 }
 
 export default Hero
