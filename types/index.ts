b6cd78932951544f88958e153a24e6e5df9c55b7
ponochovny <ponochovny@gmail.com>
2023-06-27 05:37:06 +0300

    feat: search bar & search manufacturer

--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,8 @@ export interface CustomButtonProps {
 	handleClick?: MouseEventHandler<HTMLButtonElement>
 	btnType?: 'button' | 'submit'
 }
+
+export interface SarchManufacturerProps {
+	manufacturer: string
+	setManufacturer: (manufacturer: string) => void
+}
