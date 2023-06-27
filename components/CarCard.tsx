484a9c6f5435e42484560bc5dfba4ccf3c92b48f
ponochovny <ponochovny@gmail.com>
2023-06-28 02:58:45 +0300

    feat: card details

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image'
 import { CarProps } from '@/types'
 import CustomButton from './CustomButton'
 import { calculateCarRent } from '@/utils'
+import CarDetails from './CarDetails'
 
 interface CarCardProps {
 	car: CarProps
@@ -80,6 +81,12 @@ const CarCard = ({ car }: CarCardProps) => {
 					/>
 				</div>
 			</div>
+
+			<CarDetails
+				isOpen={isOpen}
+				closeModal={() => setIsOpen(false)}
+				car={car}
+			/>
 		</div>
 	)
 }
